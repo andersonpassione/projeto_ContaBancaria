b9a0202a6e375a0ba99384fc01c49280d107994a
andersonpassione <andersonpassione@gmail.com>
2023-04-25 06:42:08 -0300

    Finalizando formatação do projeto e adicionando a operação de transferir

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -29,38 +29,60 @@ public class ContaBancaria {
             System.out.println("3 - Transferir valor");
             System.out.println("4 - Sair");
             System.out.println();
+            System.out.println("Digite a opção desejada:");
             numeroMenuOpcoes = scanner.nextInt();
-
 
             //Adicionando interação com o Menu
             switch (numeroMenuOpcoes) {
                 case 1:
-                    System.out.println();
+                    System.out.println("--------------SALDO------------");
                     System.out.println("Saldo da conta: R$" + saldoConta);
-                    System.out.println();
+                    System.out.println("-------------------------------");
                     break;
 
                 case 2:
-                    System.out.println("Informe o valor a ser depositado");
+                    System.out.println("-----------DEPOSITAR-------------");
+                    System.out.println("Informe o valor a ser depositado:");
                     double valorDepositado = scanner.nextDouble();
+
                     if (valorDepositado > 0) {
                         saldoConta += valorDepositado;
                         System.out.println("Saldo atual: R$" + saldoConta);
+                        System.out.println("------------------------------");
                         break;
                     } else {
                         System.out.println("Valor inválido");
+                        System.out.println("-------------------------------");
                         break;
                     }
 
                 case 3:
+                    System.out.println("----------TRANSFERIR-----------");
+                    System.out.println("Informe o valor de trânsferência:");
+                    double valorTransferencia = scanner.nextDouble();
+
+                    if (valorTransferencia > saldoConta){
+                        System.out.println("Saldo insuficiente para realizar a transferência");
+                        System.out.println("-------------------------------");
+                        break;
+                    } else {
+                        saldoConta -= valorTransferencia;
+                        System.out.println("Valor transferido: R$" + valorTransferencia);
+                        System.out.println("Saldo atual: R$" + saldoConta);
+                        System.out.println("-------------------------------");
+                        break;
+                    }
 
                 case 4:
-                    System.out.println();
+                    System.out.println("---------ENCERRAR--------------");
                     System.out.println("Finalizando");
-                    System.out.println();
+                    System.out.println("-------------------------------");
                     break;
+
                 default:
+                    System.out.println("-------------------------------");
                     System.out.println("Número/caractere inválido, digite novamente");
+                    System.out.println("-------------------------------");
             }
         }
 
